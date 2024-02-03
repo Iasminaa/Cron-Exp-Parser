775593f2054d11932ea79371d6a80b0a5303e19e
iasmina <pasoteiasmina@yahoo.com>
2024-02-03 20:10:22 +0000

    first pass

---
+++ CronExpParser/Parser.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CronExpParser
+{
+    public class Parser
+    {
+        public Parser() { }
+        private static string ParseSlashStar(string input, int unitOfTime, int offset) 
+        {
+            var result = new StringBuilder();
+
+            bool isStar = input.Equals("*");
+            bool startsWithSlashStar = input.StartsWith("*/");
+  
+            if (startsWithSlashStar)
+            {
+                var str = input.Split("*/");
+                bool success = Int32.TryParse(str[1], out var time);
+                if (success)
+                {
+                    var executeAmountOfTimes = unitOfTime / time;
+                    var timeResult = 0;
+                    for (int i = 0; i < executeAmountOfTimes; i++)
+                    {
+                        result.Append(timeResult);
+                        result.Append(' ');
+                        timeResult += time;
+                    }
+                }
+            }
+            else if (isStar)
+            {
+                    for (int i = offset; i < unitOfTime + offset; i++)
+                    {
+                        result.Append(i);
+                        result.Append(' ');
+                    } 
+            }
+            else
+            {
+                return input; 
+            }
+         
+            return result.ToString(); 
+        }
+        public static string ParseMinutes(string input)
+        {
+            return ParseSlashStar(input, 60, 1);
+        }
+        public static string ParseHours(string input) 
+        {
+            return ParseSlashStar(input, 24, 0);
+        }
+        public static string ParseDayOfMonth(string input) 
+         {
+            return String.Join(' ',input.Split(","));
+        }
+        public static string ParseMonth(string input)
+        {
+            return ParseSlashStar(input, 12, 1);
+        }
+        public static string ParseDayOfWeek(string input)
+        {
+            var str = input.Split("-");
+            bool success = Int32.TryParse(str[0], out var start);
+            bool success2 = Int32.TryParse(str[1], out var n);
+            var result = new StringBuilder();
+            if (success && success2)
+            {
+                for(int i = start; i <= n; i++)
+                { 
+                        result.Append(i);
+                        result.Append(' ');
+                }
+            }
+            return result.ToString();
+        }
+    }
+   
+}
---
+++ CronExpParser/Program.cs
@@ -0,0 +1,34 @@
+﻿using CronExpParser;
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        var result = new List<string>(); 
+        var cmd = "*/15 0 1,15 * 1-5 /usr/bin/find"; //Console.ReadLine
+        //var cmd = "*/15 * 1,15 * 1-5 /usr/bin/find";
+        var cmds = cmd.Split();
+
+        result.Add(Parser.ParseMinutes(cmds[0]));
+        result.Add(Parser.ParseHours(cmds[1]));
+        result.Add(Parser.ParseDayOfMonth(cmds[2]));
+        result.Add(Parser.ParseMonth(cmds[3]));
+        result.Add(Parser.ParseDayOfWeek(cmds[4]));
+        result.Add(cmds[5]);
+
+        /* ==== Output ==== */
+        var header = new string[] { "minute", "hour", "day of month", "month", "day of week", "command" }; 
+        string format = "{0,-20} {1,-20}" + Environment.NewLine;
+        var stringBuilder = new StringBuilder();
+        for (int i = 0; i < result.Count(); i++)
+        {
+            stringBuilder.AppendFormat(format, header[i], result[i]);
+        }
+
+        Console.WriteLine(stringBuilder.ToString());
+
+    }
+}
+       
